83379b3476d495c09c2c63ed8275f9ba31875179
AOA19 <aakinyemi19@gmail.com>
2024-08-07 16:58:46 -0400

    Added playist page component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Welcome from "./pages/Welcome";
 import Home from "./pages/Home";
+import Playlists from "./pages/Playlists";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/playlists" element={<Playlists />} />
       </Routes>
     </BrowserRouter>
   );
