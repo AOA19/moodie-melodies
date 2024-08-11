da43ca5a10b88600e007d916f6e5c812af978e2a
AOA19 <aakinyemi19@gmail.com>
2024-08-11 17:14:46 -0400

    Added onclick function to mood icons that navigate to playlists page. Updated styling on mood icons

--- src/components/buttons/MoodIcons.jsx
+++ src/components/buttons/MoodIcons.jsx
@@ -1,18 +1,29 @@
+import { useNavigate } from "react-router-dom";
 import { moods } from "../../data/mood-data";
 import "./MoodIcons.css";
 
 export default function MoodIcons() {
+  const navigate = useNavigate();
+
+  const handleMoodClick = (mood) => {
+    navigate(`/playlists/${mood}`);
+  }
+
+
   return (
     <div>
-      <div className="grid grid-cols-3 gap-8 px-5 pt-9">
+      <div className="grid grid-cols-2 gap-8 px-5 pt-9">
         {moods.map((mood) => (
-          <div
+          <button
             key={mood.id}
-            className="icon-bg rounded-full bg-palegreen flex flex-col justify-center items-center"
+            className="icon-bg rounded-full flex flex-col justify-center items-center m-auto bg-palegreen"
+            onClick={() => handleMoodClick(mood.mood)}
           >
-            <img src={mood.src} alt={mood.alt} />
-            <p className="text-center">{mood.mood}</p>
-          </div>
+            <img src={mood.src} alt={mood.alt} className="icon-img" />
+            <p className="text-center capitalize text-charcoal pt-2 icon-txt">
+              {mood.mood}
+            </p>
+          </button>
         ))}
       </div>
     </div>
